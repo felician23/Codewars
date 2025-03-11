acdcf57af5869f581cfdfcb7967e5085ce1d85ca
Davon Daniels <38470284+felician23@users.noreply.github.com>
2025-03-11 09:18:51 -0500

    Add files via upload

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,16 @@
+// Multiply
+
+/*
+
+This code does not execute properly. Try to figure out why.
+
+            function multiply(a,b) {
+            
+            }
+
+*/
+
+            // (Date - 3/11/2025)
+            function multiply(a,b) {
+                return a * b;
+            }

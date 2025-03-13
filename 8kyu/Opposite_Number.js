6a9bd5c9c3d4d15b4b3ca5e82983db5ae92495cb
Davon Daniels <38470284+felician23@users.noreply.github.com>
2025-03-13 09:18:01 -0500

    Add files via upload

---
+++ 8kyu/Opposite_Number.js
@@ -0,0 +1,14 @@
+// Opposite Number
+
+/*
+
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Ex. 1 : -1
+   14 : -14
+*/
+
+function opposite(number) {
+    return (-number);
+}
+
